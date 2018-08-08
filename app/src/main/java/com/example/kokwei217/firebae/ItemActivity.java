ff544290a75ecab21471fc02fed772bee3e697e0
kokwei217 <40696840+kokwei217@users.noreply.github.com>
2018-08-08 20:57:37 +0800

    Firebase Data

--- app/src/main/java/com/example/kokwei217/firebae/ItemActivity.java
+++ app/src/main/java/com/example/kokwei217/firebae/ItemActivity.java
@@ -1,51 +1,246 @@
 package com.example.kokwei217.firebae;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.renderscript.Sampler;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class ItemActivity extends AppCompatActivity {
 
     private FirebaseAuth auth;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+    private EditText remarks_ET;
+    private TextView data_TV;
+    private int u = 0;
+    private int i = 0;
+    private int j = 0;
+    private static final String TAG = "TEST";
+    private StringBuilder sb;
+    private ArrayList<String> listOfRoom;
+    private ArrayList<WifiFingerprints> wifiFingerprints;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        remarks_ET = findViewById(R.id.remarks_item);
+        data_TV = findViewById(R.id.data_tv);
         setSupportActionBar(toolbar);
         auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        listOfRoom = new ArrayList<>();
+        wifiFingerprints = new ArrayList<>();
+        myRef = database.getReference();
+        sb = new StringBuilder();
+        myRef.addChildEventListener(roomListener);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-//    @Override
-//    public void onBackPressed() {
-//        Intent a = new Intent(Intent.ACTION_MAIN);
-//        a.addCategory(Intent.CATEGORY_HOME);
-//        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//        startActivity(a);
-//    }
-
-    public void signOut(View view){
+    public void signOut(View view) {
         auth.signOut();
         finish();
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
+    public void writeDatabase(View view) {
+//        myRef.setValue(remarks_ET.getText().toString());
+//        String userID = myRef.push().getKey();
+////        writeNewUser(userID, "Kok Wei", remarks_ET.getText().toString());
+////        Toast.makeText(this, "success", Toast.LENGTH_SHORT).show();
+        StringBuilder roomSB = new StringBuilder();
 
+        for (String room : listOfRoom) {
+            roomSB.append(room + "\n");
+        }
+        data_TV.setText(roomSB);
+
+    }
+
+    private void writeNewUser(String userId, String name, String email) {
+        for (int i = 0; i < 5; i++) {
+            myRef.child("F3").child(userId).child("Fingerprints").child(String.valueOf(i)).setValue(30);
+        }
+    }
+
+    public void readDatabase(View view) {
+        listOfRoom = new ArrayList<>();
+        wifiFingerprints = new ArrayList<>();
+//        Query query = database.getReference("kokwei-1").child("F3B09-5");
+//        query.addChildEventListener(childEventListener);
+        myRef.addChildEventListener(roomListener);
+        sb = new StringBuilder();
+        u = 0;
+        j = 0;
+        Toast.makeText(this, "Read Data", Toast.LENGTH_SHORT).show();
+    }
+
+    ChildEventListener roomListener = new ChildEventListener() {
+        @Override
+        public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String s) {
+            if (snapshot.exists()) {
+                try {
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                        Log.e("TAG", "" + dataSnapshot.getKey());
+                        listOfRoom.add(dataSnapshot.getKey());
+                        if (dataSnapshot.getKey().equals(listOfRoom.get(j))) {
+                            dataSnapshot.getRef().addChildEventListener(childEventListener);
+                        }
+                        j++;
+                    }
+                } catch (Exception e) {
+                    Toast.makeText(ItemActivity.this, "error in", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            } else {
+                Log.e("TAG", " non existent");
+            }
+        }
+
+        @Override
+        public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+        }
+
+        @Override
+        public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+        }
+
+        @Override
+        public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    };
+
+    ChildEventListener dimsListener = new ChildEventListener() {
+        @Override
+        public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String s) {
+            for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                if(dataSnapshot.getKey().equals("dims")){
+                    dataSnapshot.getRef().addListenerForSingleValueEvent(fingerprintValueEventListener);
+                }
+            }
+        }
+
+        @Override
+        public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+        }
+
+        @Override
+        public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+        }
+
+        @Override
+        public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    };
+
+    ChildEventListener childEventListener = new ChildEventListener() {
+        @Override
+        public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String s) {
+            if (snapshot.exists()) {
+                try {
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+//                        Log.e("TAG", "" + dataSnapshot.getValue() + "," + dataSnapshot.getKey());// your name values you will get here
+                        if (dataSnapshot.getKey().equals("dims")) {
+                            dataSnapshot.getRef().orderByValue().addListenerForSingleValueEvent(fingerprintValueEventListener);
+                        }
+                    }
+
+
+                } catch (Exception e) {
+                    Toast.makeText(ItemActivity.this, "error in", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            } else {
+                Log.e("TAG", " non existent");
+            }
+        }
+
+        @Override
+        public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+        }
+
+        @Override
+        public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+        }
+
+        @Override
+        public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    };
+
+    ValueEventListener fingerprintValueEventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot snapshot) {
+            try {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    String bssid = dataSnapshot.getKey();
+                    String rssi = String.valueOf(dataSnapshot.getValue());
+                    WifiFingerprints fingerprints = new WifiFingerprints(bssid , rssi);
+                    wifiFingerprints.add(fingerprints);
+                    sb.append(wifiFingerprints.get(u).bssid +  wifiFingerprints.get(u).rssi  +"\n");
+                    u++;
+                }
+                sb.append(u + "," + j);
+                data_TV.setText(sb);
+
+
+            } catch (Exception e) {
+                Toast.makeText(ItemActivity.this, "error in", Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+            Log.w(TAG, "Failed to read value.", databaseError.toException());
+        }
+    };
 }
--- app/src/main/java/com/example/kokwei217/firebae/MainActivity.java
+++ app/src/main/java/com/example/kokwei217/firebae/MainActivity.java
@@ -5,8 +5,11 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,14 +19,32 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "EmailPassword";
 
     private TextView email_ET;
     private TextView password_ET;
+    private Button loginButton;
 
     private FirebaseAuth auth;
+
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            checkForEmptyFields();
+        }
+    };
 
 
     @Override
@@ -33,19 +54,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         email_ET = findViewById(R.id.email_login);
         password_ET = findViewById(R.id.password_login);
+        loginButton = findViewById(R.id.btn_login);
+
+        email_ET.addTextChangedListener(textWatcher);
+        password_ET.addTextChangedListener(textWatcher);
         email_ET.setText("kokwei217@hotmail.com");
+        password_ET.setText("xopalx217");
+        checkForEmptyFields();
 
         findViewById(R.id.btn_login).setOnClickListener(this);
+        findViewById(R.id.btn_signup).setOnClickListener(this);
 
         auth = FirebaseAuth.getInstance();
 
     }
 
+
     @Override
     protected void onStart() {
         super.onStart();
         FirebaseUser currentUser = auth.getCurrentUser();
-        if(currentUser != null){
+        if (currentUser != null) {
             Intent intent = new Intent(this, ItemActivity.class);
 //            String username = currentUser.getEmail();
 //            Toast.makeText(this, username, Toast.LENGTH_SHORT).show();
@@ -57,17 +86,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        switch(v.getId()){
+        switch (v.getId()) {
             case R.id.btn_login:
-                signIn(email_ET.getText().toString() , password_ET.getText().toString());
+                if (email_ET.getText().toString().equals("") || password_ET.getText().toString().equals("")) {
+                    Toast.makeText(this, "Please enter password/username", Toast.LENGTH_SHORT).show();
+                } else {
+                    signIn(email_ET.getText().toString(), password_ET.getText().toString());
+                }
                 break;
+            case R.id.btn_signup:
+                if (email_ET.getText().toString().equals("") || password_ET.getText().toString().equals("")) {
+                    Toast.makeText(this, "Please enter password/username", Toast.LENGTH_SHORT).show();
+                } else {
+                    signUp(email_ET.getText().toString(), password_ET.getText().toString());
+                }
             default:
                 break;
         }
 
     }
 
-    private void signIn(String email , String password){
+    private void signIn(String email, String password) {
         auth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -76,7 +115,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithEmail:success");
                             FirebaseUser user = auth.getCurrentUser();
-//                            updateUI(user);
                             Intent intent = new Intent(MainActivity.this, ItemActivity.class);
                             startActivity(intent);
                             finish();
@@ -85,12 +123,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             Log.w(TAG, "signInWithEmail:failure", task.getException());
                             Toast.makeText(MainActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
-//                            updateUI(null);
                         }
 
                         // ...
                     }
                 });
 
+    }
+
+    private void signUp(String email, String password) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = auth.getCurrentUser();
+                            Toast.makeText(MainActivity.this, "account created ", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
+    private void checkForEmptyFields() {
+        loginButton = findViewById(R.id.btn_login);
+        if (email_ET.getText().toString().equals("") || password_ET.getText().toString().equals("")) {
+            loginButton.setEnabled(false);
+        } else {
+            loginButton.setEnabled(true);
+        }
     }
 }
